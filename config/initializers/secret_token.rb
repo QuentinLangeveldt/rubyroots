7c4f3ecfb383b4c43c21766caeb0a5206fb43ee5
Quentin <quentin.langeveldt@zmartdev.co.za>
2013-01-18 12:33:43 +0200

    Initial Commit. Empty project with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyroots::Application.config.secret_token = '13ccd2334f845e3d36da383166b5bbcdc9a83444d3a474c99bca1362c5d7c9ab5ee8ac874eff6e04f292b1cbbb073a68f3f35ec6c054ee2631a2a59a7aee68be'
