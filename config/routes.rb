1bb15717d6d2ef0e28c390fd02b225eb7cb03b93
ad <ad>
2013-01-19 08:25:51 +0200

    some styling and authentication using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rubyroots::Application.routes.draw do
+  resources :tasks
+
+
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
   root :to => 'home#index'
-match '/home' => 'home#index'
+  match '/home' => 'home#index'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
